3e560b1ed0a6807d2a9f4bc7847c8a78041dc774
tharun <tharun@tharun-Vostro-3520>
2026-09-25 22:47:06 +0530

    Complete Unit 1 Task 1

---
+++ src/unit1/task1.java
@@ -0,0 +1,15 @@
+package unit1;
+/*Write a Java program that creates two boolean variables to represent whether it is
+currently the weekend and whether it is raining. Then, print both values to the console.*/
+
+public class task1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean whetherRaining=false;
+		boolean weekend=true;
+		System.out.println("Is whether raining?"+ whetherRaining +"\nIs weekend?" + weekend);	
+
+	}
+
+}
