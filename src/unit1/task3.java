b243f5ee289c2929a550b251d85f7b85ee881bd7
tharun <tharun@tharun-Vostro-3520>
2026-09-25 23:04:06 +0530

    Unit 1 Task2 Done

---
+++ src/unit1/task3.java
@@ -0,0 +1,17 @@
+package unit1;
+/*Write a Java program that uses short variables to store moderate-sized numbers representing
+geographic elevations (above and below sea level)*/
+public class task3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short mtEverest = 8848;
+		short marianaTrench = -10994;
+		System.out.println("--Geographical locations--");
+		System.out.println(" Mount Everest: " + mtEverest);
+		System.out.println(" Marina Trench" + marianaTrench);
+		
+
+	}
+
+}
