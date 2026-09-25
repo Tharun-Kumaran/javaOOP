435368a3c580c1f7272c4ba40ccf274b84c83183
tharun <tharun@tharun-Vostro-3520>
2026-09-25 22:54:59 +0530

    Unit 1 Task2 done

---
+++ src/unit1/task2.java
@@ -0,0 +1,13 @@
+package unit1;
+/*Write a Java program that uses byte variables to store small numbers representing a
+smartphone's battery level and the day of the month.*/
+public class task2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte batteryLevel=90;
+		System.out.println("Your battery level is:" + batteryLevel);
+
+	}
+
+}
