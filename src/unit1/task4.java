e8d2d5f8b45e8e433097c89c695d290b59ba9fd9
tharun <tharun@tharun-Vostro-3520>
2026-09-25 23:10:10 +0530

    Unit 1 Task4 Done

---
+++ src/unit1/task4.java
@@ -0,0 +1,14 @@
+package unit1;
+/*Write a Java program that uses int variables to track a player's high score and their total
+daily steps walked in a fitness mini-game.*/
+public class task4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int dailySteps=4500;
+		int highScore=7000;
+		
+		System.out.println("Your Steps Today: "+ dailySteps+"\nYour HighScore: "+highScore);
+	}
+
+}
